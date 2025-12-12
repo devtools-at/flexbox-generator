af2fe524d3879131c2b7b4edab2d9331ba7d96a4
DevTools.at <devtools@devtools.at>
2025-12-12 13:00:56 +0100

    Initial commit - npm package with TypeScript support

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+/**
+ * CSS Flexbox Generator
+ * Generate CSS flexbox layouts
+ *
+ * Online tool: https://devtools.at/tools/flexbox-generator
+ *
+ * @packageDocumentation
+ */
+
+
+// Core functionality for CSS Flexbox Generator
+export function encode(input: string): string {
+  // Implementation
+  return input;
+}
+
+export function decode(input: string): string {
+  // Implementation
+  return input;
+}
+
+
+// Export for convenience
+export default { encode, decode };
